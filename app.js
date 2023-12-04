c6eea75d5c3367f8c6bbc6d461fe30867cac8a63
linmumu <1054742101@qq.com>
2023-12-04 16:20:25 +0800

    修改路径获取不到grib数据，以及window环境下使用grib2json工具的路径问题

--- app.js
+++ app.js
@@ -1,110 +1,125 @@
-var express = require("express");
-var moment = require("moment");
-var http = require('http');
-var request = require('request');
-var fs = require('fs');
-var Q = require('q');
-var cors = require('cors');
+var express = require("express"); //Node.js的一个Web框架，可以用来快速构建Web应用程序
+var moment = require("moment"); //JavaScript日期处理库，可以用来格式化和解析日期。
+var http = require('http'); //Node.js的一个内置模块，可以用来创建HTTP服务器和客户端。
+var request = require('request'); //Node.js的HTTP客户端库，可以用来发送HTTP请求和处理响应。
+var fs = require('fs'); //Node.js的一个内置模块，可以用来读写文件和目录。
+var Q = require('q'); //JavaScript的Promise库，可以用来处理异步操作。
+var cors = require('cors'); //Node.js的中间件，可以用来处理跨域请求。
 
-var app = express();
+var app = express(); //构建Web应用程序
 var port = process.env.PORT || 7000;
-var baseDir ='http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl';
+
+
+var baseDir = 'http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl';
+
 
 // cors config
 var whitelist = [
-	'http://localhost:63342',
-	'http://localhost:3000',
-	'http://localhost:4000',
-	'http://danwild.github.io'
+    'http://localhost:63342',
+    'http://localhost:3000',
+    'http://localhost:4000',
+    'http://danwild.github.io'
 ];
 
 var corsOptions = {
-	origin: function(origin, callback){
-		var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
-		callback(null, originIsWhitelisted);
-	}
+    //? origin: '*', // 允许所有域名访问API
+    origin: function (origin, callback) {
+        var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+        callback(null, originIsWhitelisted);
+    }
 };
 
-app.listen(port, function(err){
-	console.log("running server on port "+ port);
+app.listen(port, function (err) {
+    console.log("running server on port " + port);
 });
+//  app.get('/user:id', function (req, res, next) {
+//     console.log('ID:', req.params.id);
+//     next();
+// }, function (req, res, next) {
+//     res.send('User Info')
+// })
 
-app.get('/', cors(corsOptions), function(req, res){
+app.get('/', cors(corsOptions), function (req, res) {
+
     res.send('hello wind-js-server.. go to /latest for wind data..');
+
+    // 如果请求不是跨域请求，req.headers.origin将返回undefined
+    // console.log("🚀 ~ name:req.headers.origin ",req.headers.origin)
 });
 
-app.get('/alive', cors(corsOptions), function(req, res){
-	res.send('wind-js-server is alive');
+app.get('/alive', cors(corsOptions), function (req, res) {
+    res.send('wind-js-server is alive');
 });
 
-app.get('/latest', cors(corsOptions), function(req, res){
+app.get('/latest', cors(corsOptions), function (req, res) {
 
-	/**
-	 * Find and return the latest available 6 hourly pre-parsed JSON data
-	 *
-	 * @param targetMoment {Object} UTC moment
-	 */
-	function sendLatest(targetMoment){
+    /**
+     * Find and return the latest available 6 hourly pre-parsed JSON data
+     *
+     * @param targetMoment {Object} UTC moment
+     * 查找最新的风速数据文件并返回给客户端。
+     * 如果当前时间点的数据文件不存在，则会尝试获取上一个时间点的数据文件，直到找到为止
+     */
+    function sendLatest(targetMoment) {
 
-		var stamp = moment(targetMoment).format('YYYYMMDD') + roundHours(moment(targetMoment).hour(), 6);
-		var fileName = __dirname +"/json-data/"+ stamp +".json";
+        var stamp = moment(targetMoment).format('YYYYMMDD') + roundHours(moment(targetMoment).hour(), 6);
+        var fileName = __dirname + "/json-data/" + stamp + ".json";
 
-		res.setHeader('Content-Type', 'application/json');
-		res.sendFile(fileName, {}, function (err) {
-			if (err) {
-				console.log(stamp +' doesnt exist yet, trying previous interval..');
-				sendLatest(moment(targetMoment).subtract(6, 'hours'));
-			}
-		});
-	}
+        res.setHeader('Content-Type', 'application/json');
+        //将数据文件发送给客户端
+        res.sendFile(fileName, {}, function (err) {
+            if (err) {
+                console.log(stamp + ' doesnt exist yet, trying previous interval..');
+                sendLatest(moment(targetMoment).subtract(6, 'hours'));
+            }
+        });
+    }
 
-	sendLatest(moment().utc());
+    sendLatest(moment().utc());
 
 });
 
-app.get('/nearest', cors(corsOptions), function(req, res, next){
+app.get('/nearest', cors(corsOptions), function (req, res, next) {
 
-	var time = req.query.timeIso;
-	var limit = req.query.searchLimit;
-	var searchForwards = false;
+    var time = req.query.timeIso;
+    var limit = req.query.searchLimit;
+    var searchForwards = false;
 
-	/**
-	 * Find and return the nearest available 6 hourly pre-parsed JSON data
-	 * If limit provided, searches backwards to limit, then forwards to limit before failing.
-	 *
-	 * @param targetMoment {Object} UTC moment
-	 */
-	function sendNearestTo(targetMoment){
+    /**
+     * Find and return the nearest available 6 hourly pre-parsed JSON data
+     * If limit provided, searches backwards to limit, then forwards to limit before failing.
+     *
+     * @param targetMoment {Object} UTC moment
+     */
+    function sendNearestTo(targetMoment) {
 
-		if( limit && Math.abs( moment.utc(time).diff(targetMoment, 'days'))  >= limit) {
-			if(!searchForwards){
-				searchForwards = true;
-				sendNearestTo(moment(targetMoment).add(limit, 'days'));
-				return;
-			}
-			else {
-				return next(new Error('No data within searchLimit'));
-			}
-		}
+        if (limit && Math.abs(moment.utc(time).diff(targetMoment, 'days')) >= limit) {
+            if (!searchForwards) {
+                searchForwards = true;
+                sendNearestTo(moment(targetMoment).add(limit, 'days'));
+                return;
+            } else {
+                return next(new Error('No data within searchLimit'));
+            }
+        }
 
-		var stamp = moment(targetMoment).format('YYYYMMDD') + roundHours(moment(targetMoment).hour(), 6);
-		var fileName = __dirname +"/json-data/"+ stamp +".json";
+        var stamp = moment(targetMoment).format('YYYYMMDD') + roundHours(moment(targetMoment).hour(), 6);
+        var fileName = __dirname + "/json-data/" + stamp + ".json";
 
-		res.setHeader('Content-Type', 'application/json');
-		res.sendFile(fileName, {}, function (err) {
-			if(err) {
-				var nextTarget = searchForwards ? moment(targetMoment).add(6, 'hours') : moment(targetMoment).subtract(6, 'hours');
-				sendNearestTo(nextTarget);
-			}
-		});
-	}
+        res.setHeader('Content-Type', 'application/json');
+        res.sendFile(fileName, {}, function (err) {
+            if (err) {
+                var nextTarget = searchForwards ? moment(targetMoment).add(6, 'hours') : moment(targetMoment).subtract(6, 'hours');
+                sendNearestTo(nextTarget);
+            }
+        });
+    }
 
-	if(time && moment(time).isValid()){
-		sendNearestTo(moment.utc(time));
-	}
-	else {
-		return next(new Error('Invalid params, expecting: timeIso=ISO_TIME_STRING'));
-	}
+    if (time && moment(time).isValid()) {
+        sendNearestTo(moment.utc(time));
+    } else {
+        return next(new Error('Invalid params, expecting: timeIso=ISO_TIME_STRING'));
+    }
 
 });
 
@@ -113,9 +128,9 @@ app.get('/nearest', cors(corsOptions), function(req, res, next){
  * Ping for new data every 15 mins
  *
  */
-setInterval(function(){
+setInterval(function () {
 
-	run(moment.utc());
+    run(moment.utc());
 
 }, 900000);
 
@@ -123,13 +138,12 @@ setInterval(function(){
  *
  * @param targetMoment {Object} moment to check for new data
  */
-function run(targetMoment){
-
-	getGribData(targetMoment).then(function(response){
-		if(response.stamp){
-			convertGribToJson(response.stamp, response.targetMoment);
-		}
-	});
+function run(targetMoment) {
+    getGribData(targetMoment).then(function (response) {
+        if (response.stamp) {
+            convertGribToJson(response.stamp, response.targetMoment);
+        }
+    });
 }
 
 /**
@@ -138,114 +152,109 @@ function run(targetMoment){
  *
  * @returns {*|promise}
  */
-function getGribData(targetMoment){
+function getGribData(targetMoment) {
 
-	var deferred = Q.defer();
+    var deferred = Q.defer();
 
-	function runQuery(targetMoment){
+    function runQuery(targetMoment) {
 
         // only go 2 weeks deep
-		if (moment.utc().diff(targetMoment, 'days') > 30){
-	        console.log('hit limit, harvest complete or there is a big gap in data..');
+        if (moment.utc().diff(targetMoment, 'days') > 30) {
+            console.log('命中极限，收获完成或数据存在较大缺口。');
             return;
         }
 
-		var stamp = moment(targetMoment).format('YYYYMMDD') + roundHours(moment(targetMoment).hour(), 6);
-		request.get({
-			url: baseDir,
-			qs: {
-				file: 'gfs.t'+ roundHours(moment(targetMoment).hour(), 6) +'z.pgrb2.1p00.f000',
-				lev_10_m_above_ground: 'on',
-				lev_surface: 'on',
-				var_TMP: 'on',
-				var_UGRD: 'on',
-				var_VGRD: 'on',
-				leftlon: 0,
-				rightlon: 360,
-				toplat: 90,
-				bottomlat: -90,
-				dir: '/gfs.'+stamp
-			}
+        var stamp = moment(targetMoment).format('YYYYMMDD') + roundHours(moment(targetMoment).hour(), 6);
+        var years = moment(targetMoment).format('YYYYMMDD')
+        var hour = roundHours(moment(targetMoment).hour(), 6);
+        request.get({
+            url: baseDir,
+            qs: {
+                file: 'gfs.t' + roundHours(moment(targetMoment).hour(), 6) + 'z.pgrb2.1p00.f000',//指定需要获取的气象预报数据文件的文件名。在这里，文件名是由多个参数组成的字符串，包括GFS模型的起报时间、预报时效、数据格式等信息。
+                lev_10_m_above_ground: 'on', //指定是否获取地面以上10米高度层的气象变量数据。这里设置为'on'，表示需要获取该高度层的数据。
+                lev_surface: 'on', //指定是否获取地面高度层的气象变量数据。这里设置为'on'，表示需要获取该高度层的数据
+                var_TMP: 'on', //指定是否获取温度（Temperature）气象变量的数据。这里设置为'on'，表示需要获取该气象变量的数据
+                var_UGRD: 'on', //指定是否获取东西向风速（Eastward Wind）气象变量的数据。这里设置为'on'，表示需要获取该气象变量的数据
+                var_VGRD: 'on', //指定是否获取南北向风速（Northward Wind）气象变量的数据。这里设置为'on'，表示需要获取该气象变量的数据
+                leftlon: 0,//从0度经线开始 获取数据
+                rightlon: 360,//到360度经线结束 获取数据
+                toplat: 90, //到90度北纬结束 获取数据
+                bottomlat: -90, //到90度南纬结束 获取数据
+                dir: '/gfs.' + years + '/' + hour + '/atmos' //dir=%2Fgfs.20231127 %2F(转义：/) stamp:时间戳
+            }
 
-		}).on('error', function(err){
-			// console.log(err);
-			runQuery(moment(targetMoment).subtract(6, 'hours'));
+        }).on('error', function (err) {
+            // console.log(err);
+            runQuery(moment(targetMoment).subtract(6, 'hours'));
 
-		}).on('response', function(response) {
+        }).on('response', function (response) {
+            console.log("🚀 ~ name:response", response.request.url.href)
+            console.log('响应状态：' + response.statusCode + ' 时间节点： ' + stamp);
 
-			console.log('response '+response.statusCode + ' | '+stamp);
+            if (response.statusCode != 200) {
+                runQuery(moment(targetMoment).subtract(6, 'hours'));
+            } else {
+                // don't rewrite stamps
+                if (!checkPath('json-data/' + stamp + '.json', false)) {
 
-			if(response.statusCode != 200){
-				runQuery(moment(targetMoment).subtract(6, 'hours'));
-			}
+                    console.log('piping ' + stamp);
 
-			else {
-				// don't rewrite stamps
-				if(!checkPath('json-data/'+ stamp +'.json', false)) {
+                    // mk sure we've got somewhere to put output
+                    checkPath('grib-data', true);
 
-					console.log('piping ' + stamp);
+                    // pipe the file, resolve the valid time stamp
+                    var file = fs.createWriteStream("grib-data/" + stamp + ".f000");
+                    response.pipe(file);
+                    file.on('finish', function () {
+                        file.close();
+                        deferred.resolve({stamp: stamp, targetMoment: targetMoment});
+                    });
 
-					// mk sure we've got somewhere to put output
-					checkPath('grib-data', true);
+                } else {
+                    console.log('already have ' + stamp + ', not looking further');
+                    deferred.resolve({stamp: false, targetMoment: false});
+                }
+            }
+        });
 
-					// pipe the file, resolve the valid time stamp
-					var file = fs.createWriteStream("grib-data/"+stamp+".f000");
-					response.pipe(file);
-					file.on('finish', function() {
-						file.close();
-						deferred.resolve({stamp: stamp, targetMoment: targetMoment});
-					});
+    }
 
-				}
-				else {
-					console.log('already have '+ stamp +', not looking further');
-					deferred.resolve({stamp: false, targetMoment: false});
-				}
-			}
-		});
-
-	}
-
-	runQuery(targetMoment);
-	return deferred.promise;
+    runQuery(targetMoment);
+    return deferred.promise;
 }
 
-function convertGribToJson(stamp, targetMoment){
+function convertGribToJson(stamp, targetMoment) {
 
-	// mk sure we've got somewhere to put output
-	checkPath('json-data', true);
+    // mk sure we've got somewhere to put output
+    checkPath('json-data', true);
 
-	var exec = require('child_process').exec, child;
+    var exec = require('child_process').exec, child;
 
-	child = exec('converter/bin/grib2json --data --output json-data/'+stamp+'.json --names --compact grib-data/'+stamp+'.f000',
-		{maxBuffer: 500*1024},
-		function (error, stdout, stderr){
+    child = exec('converter\\bin\\grib2json --data --output json-data\\' + stamp + '.json --names --compact grib-data\\' + stamp + '.f000',
+        {maxBuffer: 500 * 1024},
+        function (error, stdout, stderr) {
 
-			if(error){
-				console.log('exec error: ' + error);
-			}
+            if (error) {
+                console.log('exec error: ' + error);
+            } else {
+                console.log("converted..");
 
-			else {
-				console.log("converted..");
+                // don't keep raw grib data
+                exec('rm grib-data/*');
 
-				// don't keep raw grib data
-				exec('rm grib-data/*');
+                // if we don't have older stamp, try and harvest one
+                var prevMoment = moment(targetMoment).subtract(6, 'hours');
+                var prevStamp = prevMoment.format('YYYYMMDD') + roundHours(prevMoment.hour(), 6);
 
-				// if we don't have older stamp, try and harvest one
-				var prevMoment = moment(targetMoment).subtract(6, 'hours');
-				var prevStamp = prevMoment.format('YYYYMMDD') + roundHours(prevMoment.hour(), 6);
+                if (!checkPath('json-data/' + prevStamp + '.json', false)) {
 
-				if(!checkPath('json-data/'+ prevStamp +'.json', false)){
-
-					console.log("attempting to harvest older data "+ stamp);
-					run(prevMoment);
-				}
-
-				else {
-					console.log('got older, no need to harvest further');
-				}
-			}
-		});
+                    console.log("attempting to harvest older data " + stamp);
+                    run(prevMoment);
+                } else {
+                    console.log('got older, no need to harvest further');
+                }
+            }
+        });
 }
 
 /**
@@ -257,11 +266,12 @@ function convertGribToJson(stamp, targetMoment){
  * @param interval
  * @returns {String}
  */
-function roundHours(hours, interval){
-	if(interval > 0){
-		var result = (Math.floor(hours / interval) * interval);
-		return result < 10 ? '0' + result.toString() : result;
-	}
+
+function roundHours(hours, interval) {
+    if (interval > 0) {
+        var result = (Math.floor(hours / interval) * interval);
+        return result < 10 ? '0' + result.toString() : result;
+    }
 }
 
 /**
@@ -273,14 +283,14 @@ function roundHours(hours, interval){
  */
 function checkPath(path, mkdir) {
     try {
-	    fs.statSync(path);
-	    return true;
+        fs.statSync(path);
+        return true;
 
-    } catch(e) {
-        if(mkdir){
-	        fs.mkdirSync(path);
+    } catch (e) {
+        if (mkdir) {
+            fs.mkdirSync(path);
         }
-	    return false;
+        return false;
     }
 }
 
